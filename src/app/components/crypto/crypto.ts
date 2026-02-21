75f0eda29da5ff50ac7c00751168ab99fc8659e8
Fernando <ibanez.fernando.2018@gmail.com>
2026-02-21 16:44:19 -0300

    feat: Agregar funciones de copiar y descargar texto, mejorar la lógica de detección de cambios

--- src/app/components/crypto/crypto.ts
+++ src/app/components/crypto/crypto.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, ChangeDetectorRef } from '@angular/core';
 import { Encryption } from '../../Services/encryption';
 import { FormsModule } from '@angular/forms';
 
@@ -11,9 +11,12 @@ import { FormsModule } from '@angular/forms';
 })
 export class Crypto {
   private encryptionService = inject(Encryption);
+  constructor(private cdr: ChangeDetectorRef) { }
 
+  textoCopiado: boolean = false;
   inputText: string = '';
   outputText: string = '';
+
 
   onEncrypt() {
     const resultadoCrudo = this.encryptionService.encrypt(this.inputText);
@@ -33,5 +36,32 @@ export class Crypto {
   clear() {
     this.inputText = '';
     this.outputText = '';
+  }
+
+  downloadText() {
+    if (this.outputText) {
+      const blob = new Blob([this.outputText], { type: 'text/plain' });
+      const url = window.URL.createObjectURL(blob);
+      const a = document.createElement('a');
+      a.href = url;
+      a.download = 'resultado_matrix_crypt.txt';
+      a.click();
+      window.URL.revokeObjectURL(url); // Limpiamos la memoria
+    }
+  }
+
+  copyText() {
+    if (this.outputText) {
+      navigator.clipboard.writeText(this.outputText).then(() => {
+        this.textoCopiado = true;
+        this.cdr.detectChanges();
+        setTimeout(() => {
+          this.textoCopiado = false;
+          this.cdr.detectChanges();
+        }, 5000);
+      }).catch(err => {
+        console.error('Error al copiar: ', err);
+      });
+    }
   }
 }
